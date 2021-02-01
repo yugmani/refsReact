0e928263e2739152f6017f3d074cb749508fae76
Himalaya <yugmani@yahoo.com>
2021-01-31 22:45:13 -0800

    Error Boundary added

--- refs/src/App.js
+++ refs/src/App.js
@@ -1,16 +1,34 @@
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import RefsDemo from "./RefsDemo";
+import ErrorBoundary from "./ErrorBoundary";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <RefsDemo />
-    </div>
-  );
+import PortalDemo from "./PortalDemo";
+import Hero from "./Hero";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <RefsDemo />
+        <PortalDemo />
+        <ErrorBoundary>
+          <Hero heroName="Batman" />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Hero heroName="Superman" />
+        </ErrorBoundary>
+
+        <ErrorBoundary>
+          <Hero heroName="Terror" />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ refs/src/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Hero({ heroName }) {
+  if (heroName === "Terror") {
+    throw new Error("Not a Hero");
+  }
+  return <div>{heroName}</div>;
+}
+
+export default Hero;
